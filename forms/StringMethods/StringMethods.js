f462eb45bb3108446aaf7bb00f8d4ba3a83b4f9c
Lauren <laurenermer@hotmail.com>
2019-09-18 11:45:24 -0500

    Initial commit

---
+++ forms/StringMethods/StringMethods.js
@@ -0,0 +1,20 @@
+//A
+let quoteString= 'Failure is simply the opportunity to begin again this time more intelligently';
+//B
+let upperCaseString= quoteString.toUpperCase();
+console.log(upperCaseString);
+//C
+let authorString= '- Henry Ford'
+//D
+let completeString= quoteString.concat(authorString);
+console.log(completeString);
+//E
+let secondString= 'Talent is cheaper than table salt. What separates the talented individual from the successful one is a lot of hard work. - Stephen King'
+//F
+lowerCaseString= secondString.toLowerCase();
+console.log(lowerCaseString);
+//G
+console.log(secondString.charAt(2));
+//H
+let findString= secondString.slice(35, -14);
+console.log(findString);
